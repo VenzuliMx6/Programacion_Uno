ba3dfcd61b451defb5c169e8fcf0430a9c0b4366
tominashegodbased123 <tttttomixd@gmail.com>
2022-03-28 14:48:30 -0300

    Programacion Variables y Funciones

--- variables_funciones/index.js
+++ variables_funciones/index.js
@@ -1,7 +1,11 @@
 //Función clásica
 function mostrar() {
+  
+  /* Forma 1 */
+  const nombres = document.querySelector("#inp_nombre").value
+  
+  
+
   //Siempre que sea solamente texto usar ''
-  alert('Probando');
+  alert(nombres);
 }
-
-
